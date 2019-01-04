acd814eb54b15a663a89a4284658bf10da9cb62e
Jim Boone <jim@traveler.lan>
2019-01-03 20:48:47 -0800

    Epoch

---
+++ app/identification.php
@@ -0,0 +1,442 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+/*
+UserSpice 4
+An Open Source PHP User Management System
+by the UserSpice Team at http://UserSpice.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
+<?php
+require_once '../users/init.php';
+require_once $abs_us_root.$us_url_root.'users/includes/header.php';
+require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
+?>
+
+<?php if (!securePage($_SERVER['PHP_SELF'])){die();} ?>
+
+<?php
+//PHP Goes Here!
+?>
+<div id="page-wrapper">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-12">
+
+
+
+<h3>Identification Guide</h3>
+This is not a comprehensive list of differences between the cars
+rather a quick guide to identifying a car.  Like all manufacturers,
+there were running changes throughout the life of the Elan.  In the
+case of Lotus, many of these changes were poorly documented.  If
+in doubt, post a question to the 
+<a href="http://www.lotuselan.net/forums/">LotusElan.net forum</a> and ask.
+
+
+<ul>
+<li><a href="#roadster">Roadster</a></li>
+<li><a href="#coupe">Coupe</a></li>
+<li><a href="#race">Race</a></li>
+<li><a href="#plus2">Plus 2</a></li>
+</ul>
+
+<a name="roadster"></a>
+<fieldset><legend>Roadster/Drophead</legend>
+<table class="pme-main">
+        <tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Elan 1500</strong><br><i> Type 26 Elan 1500 Roadster </i> </td>
+		<td class="pme-cell-0">
+        <ul>
+            <li><a href="http://www.type26register.com/ ">Complete Details</a></li>
+            <li>1500 badge on boot</li>
+            <li>All were recalled from the factory and updated to Elan 1600 specification</li>
+            <li>Boot lid does not extend all the way to the trailing edge of the car</li>
+            <li>Lift up windows</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-0"></td> <!-- Image -->
+		</tr>
+    
+        <tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Elan 1600 </strong><br><i> Type 26 S1 Roadster </i>
+		</td>
+		<td class="pme-cell-1">
+        <ul>
+
+            <li><a href="http://www.type26register.com/ ">Complete Details</a></li>
+
+            <li>1600 badge on boot</li>
+			<li>Round tail lights</li>
+			<li>No rollup windows</li>
+            <li>Boot lid does not extend all the way to the trailing edge of the car</li>
+            <li>Lift up windows</li>
+        </ul>
+		</td>
+		<td class="pme-cell-1">
+		  <a href="images/examples/elan1600.jpg" rel="lightbox-type26s1roadster" title="Type 26 Elan 1600 (S1)">
+		  <img class="polaroid" src="images/examples/thumbs/elan1600.jpg" height="112" width="150" title="Type 26 Elan 1600 (S1)"></a></td>
+		</tr>
+    
+        <tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Roadster S2 </strong><br><i> Type 26 S2 Roadster </i> 
+		</td>
+		<td class="pme-cell-0">
+        <ul>
+            <li><a href="http://www.type26register.com/ ">Complete Details</a></li>
+            <li>No rollup windows</li>
+            <li>Oval tail lights.  Early cars had round lights</li>
+            <li>Boot lid does not extend all the way to the trailing edge of the car</li>
+            <li>Lift up windows</li>
+        </ul>
+		</td>
+		<td class="pme-cell-0">
+		<a href="images/examples/s2a.jpg" rel="lightbox-type26s2roadster" title="Type 26 Elan S2">
+		<img class="polaroid" src="images/examples/thumbs/s2a.jpg" height="112" width="150" title="Type 26 Elan S2"></a>
+		<a href="images/examples/s2b.jpg" rel="lightbox-type26s2roadster" title="Type 26 Elan S2">
+		<img class="polaroid" src="images/examples/thumbs/s2b.jpg" height="112" width="150" title="Type 26 Elan S2"></a>
+		</td> 
+		</tr>
+    
+    	<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Drophead S3 </strong><br><i> Type 45 S3 DHC </i>
+		</td>
+		<td class="pme-cell-1">
+    	<ul>
+            <li>Electric rollup windows with fixed frames</li>
+            <li>Oval tail lights</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-1"></td> <!-- Image -->
+		</tr>
+    
+    	<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Drophead S3 S/E </strong><br><i> Type 45 S3 S/E DHC </i>
+		</td>
+		<td class="pme-cell-0">
+    	<ul>
+            <li>Electric rollup windows with fixed frames</li>
+            <li>Oval tail lights</li>
+            <li>S/E badged</li>
+            <li>Stainless steel side trims & wing mounted flasher repeaters on most cars</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-0">
+		<a href="images/examples/s3_se_dhc.jpg" rel="lightbox-type45s3sedhc" title="Type 45 S3 S/E DHC">
+		<img class="polaroid" src="images/examples/thumbs/s3_se_dhc.jpg" height="90" width="150" title="Type 45 S3 S/E DHC"></a>
+		</td>
+		</tr>
+    
+    	<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Drophead S4  </strong><br><i> Type 45 S4 DHC </i>
+		</td>
+		<td class="pme-cell-1">
+    	<ul>
+            <li>Square tail lights</li>
+            <li>Square profile wheelarches</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-1">
+		<a href="images/examples/s4_dhc.jpg" rel="lightbox" title="Type 45 S4 DHC">
+		<img class="polaroid" src="images/examples/thumbs/s4_dhc.jpg" height="89" width="150" title="Type 45 S4 DHC"></a>
+		</td> 
+		</tr>
+    
+    	<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Drophead S4 S/E  </strong><br><i> Type 45 S4 S/E DHC </i>
+		</td>
+		<td class="pme-cell-0">
+    	<ul>
+            <li>Square tail lights</li>
+            <li>Square profile wheelarches</li>
+            <li>S/E badged</li>
+            <li>Stainless steel side trims & wing mounted flasher repeaters on most cars</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-0">
+		<a href="images/examples/s4_se_dhc_b.jpg" rel="lightbox-type45s4sedhc" title="Type 45 S4 S/E DHC">
+		<img class="polaroid" src="images/examples/thumbs/s4_se_dhc_b.jpg" height="112" width="150" title="Type 45 S4 S/E DHC"></a>
+		<a href="images/examples/s4_se_dhc_a.jpg" rel="lightbox-type45s4sedhc" title="Type 45 S4 S/E DHC">
+		<img class="polaroid" src="images/examples/thumbs/s4_se_dhc_a.jpg" height="112" width="150" title="Type 45 S4 S/E DHC"></a>
+		</td>
+		</tr>
+    
+    	<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Drophead Sprint </strong><br><i> Type 45 Sprint DHC </i>
+		</td>
+		<td class="pme-cell-1">
+    	<ul>
+            <li><a href="http://www.lotuselansprint.com">Complete Details</a></li>
+            <li>Square tail lights</li>
+            <li>Badged as Sprint</li>
+            <li>Unique Two tone paint.  Could be deleted as an option</li>
+            <li>Square profile wheelarches</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-1">
+		<a href="images/examples/sprint_dhc.jpg" rel="lightbox" title="Type 45 Sprint DHC">
+		<img class="polaroid" src="images/examples/thumbs/sprint_dhc.jpg" height="84" width="150" title="Type 45 Sprint DHC"></a>
+		</td> 
+		</tr>
+	</table>
+	</fieldset>
+    
+<a name="coupe"></a>
+<fieldset><legend>Coupe</legend><br>
+<table class="pme-main">
+    	<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Coupe S3 Pre-Airflow </strong><br><i> Type 36 FHC-preairflow </i>
+		</td>
+		<td class="pme-cell-0">
+    	<ul>
+            <li>Oval tail lights</li>
+            <li>No extractor grill on the B pillar</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-0">
+		<a href="images/examples/s3_fhc_pre.jpg" rel="lightbox" title="Type 36 FHC preairflow">
+		<img class="polaroid" src="images/examples/thumbs/s3_fhc_pre.jpg" height="99" width="150" title="Type 36 FHC preairflow"></a>
+		</td> 
+		</tr>
+    
+    	<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Coupe S3 Airflow </strong><br><i> Type 36 S3 FHC </i>
+		</td>
+		<td class="pme-cell-1">
+    	<ul>
+            <li>Oval tail lights</li>
+            <li>Extractor grill on the rear quarter panel</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-0">
+		<a href="images/examples/s3_fhc_air.jpg" rel="lightbox" title="Type 36 FHC">
+		<img class="polaroid" src="images/examples/thumbs/s3_fhc_air.jpg" height="100" width="150" title="Type 36 FHC"></a>
+		</td> 
+		</tr>
+    
+    	<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Coupe S3 S/E Airflow </strong><br><i> Type 36 S3 S/E FHC </i>
+		</td>
+		<td class="pme-cell-0">
+    	<ul>
+            <li>Oval tail lights</li>
+            <li>Extractor grill on the rear quarter panel</li>
+            <li>S/E badged</li>
+            <li>Stainless steel side trims & wing mounted flasher repeaters on most cars</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-0"></td> <!-- Image -->
+		</tr>
+    
+    	<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Coupe S4 </strong><br><i> Type 36 S4 FHC </i>
+		</td>
+		<td class="pme-cell-1">
+    	<ul>
+            <li>Square tail lights</li>
+            <li>Square profile wheelarches</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-1">
+		<a href="images/examples/s4_fhc.jpg" rel="lightbox" title="Type 36 S4 FHC">
+		<img class="polaroid" src="images/examples/thumbs/s4_fhc.jpg" height="79" width="150" title="Type 36 S4 FHC"></a>
+		</td> <!-- Image -->
+		</tr>
+    
+    	<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Coupe S4 S/E </strong><br><i> Type 36 S4 S/E FHC </i>
+		</td>
+		<td class="pme-cell-0">
+    	<ul>
+            <li>Square tail lights</li>
+            <li>Square profile wheelarches</li>
+            <li>S/E badged</li>
+            <li>Stainless steel side trims & wing mounted flasher repeaters on most cars</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-0"></td> <!-- Image -->
+		</tr>
+    
+    	<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Coupe Sprint </strong><br><i> Type 36 Sprint FHC </i>
+		</td>
+		<td class="pme-cell-1">
+    	<ul>
+            <li><a href="http://www.lotuselansprint.com">Complete Details</a></li>
+            <li>Square tail lights</li>
+            <li>Badged as Sprint</li>
+            <li>Unique Two tone paint.  Could be deleted as an option</li>
+            <li>Square profile wheelarches</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-1">
+		<a href="images/examples/sprint_fhc.jpg" rel="lightbox" title="Type 45 Sprint DHC">
+		<img class="polaroid" src="images/examples/thumbs/sprint_fhc.jpg" height="56" width="150" title="Type 45 Sprint DHC"></a>
+		</td> <!-- Image -->
+		</tr>
+	</table>
+	</fieldset>
+
+<a name="race"></a>
+	<fieldset><legend>Racing Version</legend>
+<table class="pme-main">
+    	<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>26R </strong><br><i> Type 26 26R Race </i>
+		</td>
+		<td class="pme-cell-0">
+    	<ul>
+            <li>Fixed headlights, Most not all</li>
+            <li>Magnesium Lotus designed peg drive wheels</li>
+            <li>S2 versions had lightweight bodies and flared fenders</li>
+    	</ul>
+		</td>
+		<td class="pme-cell-0">
+		<a href="images/examples/26r.jpg" rel="lightbox" title="Type 26R">
+		<img class="polaroid" src="images/examples/thumbs/26r.jpg" height="83" width="150" title="Type 26R"></a>
+		</td> 
+		</tr>
+	</table>
+	</fieldset>
+    
+	
+<a name="plus2"></a>
+	<fieldset><legend>Plus 2</legend>
+<table class="pme-main">
+		<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Plus 2</strong><br><i>Type 50 +2</i>
+		</td>
+		<td class="pme-cell-0">
+		<ul>
+			<li>50/0001 on </li>
+			<li>2 large gauges - Speedometer, Tachometer</li>
+			<li>4 small gauges - Water Temp, Oil Pressure, Ammeter, Fuel</li>
+		</ul>
+		</td>
+		<td class="pme-cell-0">
+		<a href="images/examples/plus2int.jpg" rel="lightbox" title="Type 50 Plus 2 Dash">
+		<img class="polaroid" src="images/examples/thumbs/plus2int.jpg" height="112" width="150" title="Type 50 Plus 2 Dash"></a>
+		</tr>
+
+		<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Plus 2 Federal</strong><br><i>Type 50 +2 Federal</i>
+		</td>
+		<td class="pme-cell-1">
+		<ul>
+			<li>50/0857 on for US, 50/0929 all markets </li>
+			<li>2 large gauges - Speedometer, Tachometer</li>
+			<li>4 small gauges - Water Temp, Oil Pressure, Ammeter, Fuel</li>
+			<li>Remote boot release, flush interior door handles, modified exhaust</li>
+		</ul>
+		</td>
+		<td class="pme-cell-1"></td> <!-- Image -->
+		</tr>
+
+		<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Plus 2S </strong><br><i>Type 50 +2 2S</i>
+		</td>
+		<td class="pme-cell-0">
+		<ul>
+			<li>50/1593 on </li>
+			<li>2 large gauges - Speedometer, Tachometer</li>
+			<li>6 small gauges -Oil, Water temp, Battery Condition, Temp, Fuel, Clock</li>
+			<li>4 warning lights in center of dash (Hazard, Parking Brake, Brake Fail, Rear Screen)</li>
+			<li>New luxury interior including revised seats and centre console</li>
+			<li>Fog/Driving lamps below the bumper</li>
+		</ul>
+		</td>
+		<td class="pme-cell-0">
+		<a href="images/examples/plus2sa.jpg" rel="lightbox-type50plus2s" title="Type 50 Plus 2S">
+		<img class="polaroid" src="images/examples/thumbs/plus2sa.jpg" height="99" width="150" title="Type 50 Plus 2S"></a>
+		<a href="images/examples/plus2sb.jpg" rel="lightbox-type50plus2s" title="Type 50 Plus 2S">
+		<img class="polaroid" src="images/examples/thumbs/plus2sb.jpg" height="99" width="150" title="Type 50 Plus 2S"></a>
+		</td> 
+		</tr>
+
+		<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Plus 2S Federal </strong><br><i>Type 50 +2 2S Federal</i>
+		</td>
+		<td class="pme-cell-1">
+		<ul>
+			<li>50/2447 on </li>
+			<li>2 large gauges - Speedometer, Tachometer</li>
+			<li>6 small gauges -Oil, Water temp, Battery Condition, Temp, Fuel, Clock</li>
+			<li>4 warning lights in center of dash (Hazard, Parking Brake, Brake Fail, Rear Screen)</li>
+			<li>Luxury interior including revised seats and centre console</li>
+			<li>Fog/Driving lamps below the bumper</li>
+		</ul>
+		</td>
+		<td class="pme-cell-1"></td> <!-- Image -->
+		</tr>
+
+		<tr class="pme-row-0">
+		<td class="pme-cell-0"><strong>Plus 2S 130</strong><br><i>Type 50 +2 130</i>
+		</td>
+		<td class="pme-cell-0">
+		<ul>
+			<li>71.01...0001 on </li>
+			<li>2 large gauges - Speedometer, Tachometer</li>
+			<li>6 small gauges - Oil, Water temp, Battery Condition, Temp, Fuel, Clock</li>
+			<li>3 warning lights in center of dash (Hazard, Park/Brake Fail, Rear Screen) </li>
+			<li>Luxury interior including revised seats and centre console</li>
+			<li>Big Valve engine
+			<li>Fog/Driving lamps below the bumper</li>
+		</ul>
+		</td>
+		<td class="pme-cell-0">
+		  <a href="images/examples/plus2s130a.jpg" rel="lightbox-type50plus2s130" title="Type 50 Plus2S 130">
+		  <img class="polaroid" src="images/examples/thumbs/plus2s130a.jpg" height="112" width="150" title="Type 50 Plus2S 130"></a>
+		  <a href="images/examples/plus2s130b.jpg" rel="lightbox-type50plus2s130" title="Type 50 Plus2S 130">
+		  <img class="polaroid" src="images/examples/thumbs/plus2s130b.jpg" height="112" width="150" title="Type 50 Plus2S 130"></a>
+		  <a href="images/examples/plus2s130int.jpg" rel="lightbox-type50plus2s130" title="Type 50 Plus2S 130">
+		  <img class="polaroid" src="images/examples/thumbs/plus2s130int.jpg" height="112" width="150" title="Type 50 Plus2S 130"></a>
+		  <a href="images/examples/plus2s130d.jpg" rel="lightbox-type50plus2s130" title="Type 50 Plus2S 130">
+		  <img class="polaroid" src="images/examples/thumbs/plus2s130d.jpg" height="112" width="150" title="Type 50 Plus2S 130"></a>
+		</td>
+		</tr>
+
+		<tr class="pme-row-1">
+		<td class="pme-cell-1"><strong>Plus 2S 130/5</strong><br><i>Type 50 +2 130/5</i>
+		</td>
+		<td class="pme-cell-1">
+		<ul>
+			<li>72.10... on </li>
+			<li>Same as Plus 2S 130 with 5-speed gearbox</li>
+		</ul>
+		</td>
+		<td class="pme-cell-1"></td> <!-- Image -->
+		</tr>
+	</table>
+	</fieldset>
+
+
+
+
+
+					<!-- End of main content section -->
+			</div> <!-- /.col -->
+		</div> <!-- /.row -->
+	</div> <!-- /.container -->
+</div> <!-- /.wrapper -->
+
+
+	<!-- footers -->
+<?php require_once $abs_us_root.$us_url_root.'users/includes/page_footer.php'; // the final html footer copyright row + the external js calls ?>
+
+<!-- Place any per-page javascript here -->
+
+<?php require_once $abs_us_root.$us_url_root.'users/includes/html_footer.php'; // currently just the closing /body and /html ?>
