0a141fd0e6d83165f209e81c2f977587f759b7c7
Jim Boone <jim.unibrain.org@gmail.com>
2019-01-04 18:19:46 -0800

    Delete sync.sh

--- sync.sh
+++
@@ -1,5 +0,0 @@
-cd /Users/jim/Documents/Working/Registry
-
-rsync -avzhe ssh --delete elanregi@elanregistry:/home/elanregi/test/usersc .
-rsync -avzhe ssh --delete elanregi@elanregistry:/home/elanregi/test/TODO .
-rsync -avzhe ssh --delete --exclude 'userimages' elanregi@elanregistry:/home/elanregi/test/app .
