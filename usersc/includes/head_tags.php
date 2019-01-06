8c5dbd814ae6367180f4823ee0388eb8e1b42d5b
Jim Boone <jim@traveler.lan>
2019-01-06 10:30:25 -0800

    Add car model definition js Updated page author, web, and description tags

--- usersc/includes/head_tags.php
+++ usersc/includes/head_tags.php
@@ -1,6 +1,13 @@
-<!-- Facebook sharing meta tags (delete if you don't want them) -->
-<meta property="og:url"                content="" /> <!-- URL for website (link address) -->
+<meta property="og:url"                content="https://www.elanregistry.org/" /> <!-- URL for website (link address) -->
 <meta property="og:type"               content="website" /> <!-- type of site -->
-<meta property="og:title"              content="Userspice Site" /> <!-- title of site (title of share) -->
-<meta property="og:description"        content="Userspice Site Description" /> <!-- description of site (text which appears when sharing) -->
+<meta property="og:title"              content="Lotus Elan Registry" /> <!-- title of site (title of share) -->
+<meta property="og:description"        content="Registry for the Lotus Elan and Elan +2" /> <!-- description of site (text which appears when sharing) -->
 <meta property="og:image"              content="" /> <!-- URL for preview image -->	
+
+<!-- Add car validation JS -->
+<!-- TODO this should be only on tha pages needed -->
+<?php
+	if(file_exists($abs_us_root.$us_url_root.'app/js/cardefinition.js')){
+			?>	 <script language="JavaScript" src=<?=$us_url_root.'app/js/cardefinition.js'?> type="text/javascript"></script> <?php
+	}
+?>
