7cef3d306dc087a839ca816b7ef3eb4a16998b6f
Jim Boone <jim@unibrain.org>
2019-01-17 07:44:58 -0800

    Create conversion script per issue #3.   Convert2.php will convert existing DB.  Will need to update password in the file for existing DB. Cleanup.sql "undoes" the conversion - Not perfect but good for testing. db_translae.xlsx is the cross reference for the translation
    Updated logo  file to make a little smaller

---
+++ SQL/cleanup.sql
@@ -0,0 +1,5 @@
+DELETE FROM 	users WHERE id > 20
+DELETE FROM 	cars WHERE id > 20
+DELETE FROM 	car_user WHERE id > 20
+DELETE FROM cars_hist WHERE id > 20
+DELETE FROM profiles WHERE `user_id` > 20
