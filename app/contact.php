1c108b560990b9db9ae3b6723eb8caa72db7ad13
Jim Boone <jim@unibrain.org>
2019-01-17 19:16:04 -0800

    Added comment form per #46

---
+++ app/contact.php
@@ -0,0 +1,87 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+/*
+UserSpice 4
+An Open Source PHP User Management System
+by the UserSpice Team at http://UserSpice.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+?>
+<?php
+require_once '../users/init.php';
+require_once $abs_us_root.$us_url_root.'users/includes/header.php';
+require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
+?>
+
+<?php if (!securePage($_SERVER['PHP_SELF'])){die();} ?>
+
+<?php
+//PHP Goes Here!
+
+?>
+<div id="page-wrapper">
+	<div class="container-fluid">
+<div class="row">
+	<div class="col-xs-12">
+
+		<div class="jumbotron">
+					<!-- Content Goes Here. Class width can be adjusted -->
+
+<form name="contactform" method="post" action="send_form_email.php">
+	<table>
+	<tr> <td width="25%"> <label> First Name </label> </td>  <td> <?php echo $user->data()->fname;?> </td> </tr>
+	<tr> <td width="25%"> <label> Last Name </label> </td>  <td> <?php echo $user->data()->lname;?> </td> </tr>
+	<tr> <td width="25%"> <label> Email </label> </td>  <td> <?php echo $user->data()->email;?> </td> </tr>
+	<tr> <td width="25%"> <label> Account ID </label> </td>  <td> <?php echo $user->data()->id;?> </td> </tr>
+	
+
+	 <td valign="top">
+	  <label for="comments">Comments</label>
+	 </td>
+	 <td valign="top">
+	  <textarea  name="comments" maxlength="1000" cols="60" rows="10"></textarea>
+	 </td>
+	</tr>
+	<tr>
+	 <td colspan="2" style="text-align:center">
+	 <input type="hidden" name="fname" value="<?php echo $user->data()->fname;?>" />
+	 <input type="hidden" name="lname" value="<?php echo $user->data()->lname;?>" />
+	 <input type="hidden" name="email" value="<?php echo $user->data()->email;?>" />
+	 <input type="hidden" name="id" value="<?php echo $user->data()->id;?>" />
+
+	 <input class='btn btn-primary' type='submit' value='submit' class='submit' /></p>
+	 </td>
+	</tr>
+</table>
+</form>
+
+
+
+				</div>	<!-- End of jumbotron content section -->
+			</div> <!-- /.col -->
+		</div> <!-- /.row -->
+	</div> <!-- /.container -->
+</div> <!-- /.wrapper -->
+
+
+	<!-- footers -->
+<?php require_once $abs_us_root.$us_url_root.'users/includes/page_footer.php'; // the final html footer copyright row + the external js calls ?>
+
+<!-- Place any per-page javascript here -->
+
+<?php require_once $abs_us_root.$us_url_root.'users/includes/html_footer.php'; // currently just the closing /body and /html ?>
---
+++ app/send_form_email.php
@@ -0,0 +1,125 @@
+
+
+<?php
+require_once '../users/init.php';
+require_once $abs_us_root.$us_url_root.'users/includes/header.php';
+require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
+?>
+
+<?php if (!securePage($_SERVER['PHP_SELF'])){die();} ?>
+
+<?php
+//PHP Goes Here!
+
+if(isset($_POST['email'])) {
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "jim@unibrain.org";
+    $email_subject = "[ELANREGISTRY] Feedback";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+    // validation expected data exists
+    if(!isset($_POST['fname']) ||
+        !isset($_POST['lname']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['id']) ||
+        !isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+    
+ 
+    $first_name = $_POST['fname']; // required
+    $last_name = $_POST['lname']; // required
+    $email_from = $_POST['email']; // required
+    $id_from = $_POST['id']; // required
+    $comments = $_POST['comments']; // required
+ 
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$email_from)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+ 
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+  if(!preg_match($string_exp,$first_name)) {
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(!preg_match($string_exp,$last_name)) {
+    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+  }
+ 
+  if(strlen($comments) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+ 
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+ 
+    $email_message = "Feedback below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+    $email_message .= "First Name : ".clean_string($first_name)."\n";
+    $email_message .= "Last Name  : ".clean_string($last_name)."\n";
+    $email_message .= "Email      : ".clean_string($email_from)."\n";
+    $email_message .= "Account ID : ".clean_string($id_from)."\n\n";
+    $email_message .= "Comments   :\n".clean_string($comments)."\n";
+ 
+// create email headers
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+}
+
+?>
+<div id="page-wrapper">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-sm-12">
+            <div class="jumbotron">
+
+              <!-- Content Goes Here. Class width can be adjusted -->
+
+            <h2><p>Thank you for feedback!  Your help makes the Elan Registry better!</p></h2>
+            <h3><p>Taking you back home in a few secondss</p></h3>
+
+            <script>
+            //Using setTimeout to execute a function after 5 seconds.
+            setTimeout(function () {
+               //Redirect with JavaScript
+               window.location.href= '/';
+            }, 5000);
+            
+            </script>
+          </div><!-- End of main content section -->
+      </div> <!-- /.col -->
+    </div> <!-- /.row -->
+  </div> <!-- /.container -->
+</div> <!-- /.wrapper -->
+
+
+  <!-- footers -->
+<?php require_once $abs_us_root.$us_url_root.'users/includes/page_footer.php'; // the final html footer copyright row + the external js calls ?>
+
+<!-- Place any per-page javascript here -->
+
+<?php require_once $abs_us_root.$us_url_root.'users/includes/html_footer.php'; // currently just the closing /body and /html ?>
