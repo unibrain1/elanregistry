9d82156ce0c628c92c3228e772b0aece944884df
Jim Boone <jim@unibrain.org>
2019-01-19 20:33:07 -0800

    Removed dependency on phpMyAdmin  Issue #9, #14 Replaced tables with datatables jquery Issue #8 Date format change to YYYY-MM-DD Issue #68

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
+if(file_exists("install/index.php")){
+	//perform redirect if installer files exist
+	//this if{} block may be deleted once installed
+	header("Location: install/index.php");
+}
+
 require_once 'users/init.php';
 require_once $abs_us_root.$us_url_root.'users/includes/header.php';
 require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
