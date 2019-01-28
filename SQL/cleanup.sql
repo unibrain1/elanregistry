c1706861f8e4a8a68e979658602bb163ce45ce9c
Jim Boone <jim@unibrain.org>
2019-01-27 20:33:51 -0800

    Reset car history and auto increment

--- SQL/cleanup.sql
+++ SQL/cleanup.sql
@@ -3,3 +3,10 @@ DELETE FROM elanregi_spice.cars WHERE id > 20;
 DELETE FROM elanregi_spice.car_user WHERE id > 20;
 DELETE FROM elanregi_spice.cars_hist WHERE id > 20;
 DELETE FROM elanregi_spice.profiles WHERE `user_id` > 20
+DELETE FROM elanregi_spice.cars_hist WHERE tid > 20;
+
+
+
+ALTER TABLE elanregi_spice.users  AUTO_INCREMENT = 10
+ALTER TABLE elanregi_spice.cars  AUTO_INCREMENT = 10
+
