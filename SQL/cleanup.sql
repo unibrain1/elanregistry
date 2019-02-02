7df08453f2c92913d6836690d25d7c24651bc5ed
Jim Boone <jim@unibrain.org>
2019-02-02 07:19:35 -0800

    Cleanup now works

--- SQL/cleanup.sql
+++ SQL/cleanup.sql
@@ -1,12 +1,10 @@
 DELETE FROM elanregi_spice.users WHERE id > 20;
-DELETE FROM elanregi_spice.cars WHERE id > 20;
-DELETE FROM elanregi_spice.car_user WHERE id > 20;
-DELETE FROM elanregi_spice.cars_hist WHERE id > 20;
-DELETE FROM elanregi_spice.profiles WHERE `user_id` > 20
-DELETE FROM elanregi_spice.cars_hist WHERE tid > 20;
+DELETE FROM elanregi_spice.profiles WHERE `user_id` > 20;
+DELETE FROM elanregi_spice.cars WHERE id > 0;
+DELETE FROM elanregi_spice.car_user WHERE id > 0;
+DELETE FROM elanregi_spice.cars_hist WHERE id > 0;
 
 
 
-ALTER TABLE elanregi_spice.users  AUTO_INCREMENT = 10
-ALTER TABLE elanregi_spice.cars  AUTO_INCREMENT = 10
-
+ALTER TABLE elanregi_spice.users  AUTO_INCREMENT = 10;
+ALTER TABLE elanregi_spice.cars  AUTO_INCREMENT = 10;
