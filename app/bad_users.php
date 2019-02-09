c31b04db1b54e91bc7d447cee9b724d6ad0c7565
Jim Boone <jim@unibrain.org>
2019-02-09 15:51:40 -0800

    Added display of carID in users list. Added basic script to remove users without cars.  These are SPAM accounts

---
+++ app/bad_users.php
@@ -0,0 +1,31 @@
+<?php
+ // Remove users with no reason to be here!
+
+require_once '../users/init.php';
+require_once $abs_us_root.$us_url_root.'users/includes/header.php';
+require_once $abs_us_root.$us_url_root.'users/includes/navigation.php';
+
+if (!securePage($_SERVER['PHP_SELF'])) {
+    die();
+}
+
+
+
+$usersQ = $db->query("SELECT  id as userid FROM  users WHERE  users.id NOT IN (SELECT userid FROM car_user)");
+
+if ($usersQ->count() > 0) {
+    $users = $usersQ->results();
+}
+
+
+foreach ($users as $u) {
+	echo "<p>Remove userID ". $u->userid."</p>";
+	deleteUsers(array($u->userid));
+
+}
+
+
+
+
+
+?>
