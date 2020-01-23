bbc3632fd1948494e443bf5c30195f9eb19921eb
Jim Boone <jim@unibrain.org>
2020-01-23 10:45:47 -0800

    Updated logic to remove bad/spam users.  - No Car  - Email not verified  - No login  - Older than 30 days

--- app/bad_users.php
+++ app/bad_users.php
@@ -1,26 +1,40 @@
 <?php
- // Remove users with no reason to be here!
 
-require_once '../users/init.php';
-require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
+/*
+ Delete SPAM users.  A SPAM users is one where the email is not verified, as never logged in, has no CAR and the join_date is over 30 days old.
 
-if (!securePage($_SERVER['PHP_SELF'])) {
-    die();
+
+ */
+
+ require_once '../users/init.php';
+
+
+$q="
+SELECT  users.id
+FROM users
+LEFT JOIN car_user
+ON (users.id = car_user.userid)
+where ( users.email_verified = 0 AND users.last_login = 0 AND car_user.carid is NULL AND users.join_date  < CURRENT_DATE - INTERVAL 30 DAY)
+GROUP BY users.id 
+";
+
+
+// The DB for SPICE should already be open!
+$db = DB::getInstance();
+
+
+// This should update all the users to make sure they have a profile
+$usersQ = $db->query( $q );
+
+echo "Delete ". $usersQ->count() ." SPAM users</br>";
+
+$users = $usersQ->results();
+foreach($users as $u)
+{
+	echo "- user_id ". $u->id ."</br>" ;
+	deleteUsers(array($u->id));
+	$db->query("DELETE FROM profiles WHERE user_id = ?",array($u->id));
 }
 
-
-
-$usersQ = $db->query("SELECT  id as userid FROM  users WHERE  users.id NOT IN (SELECT userid FROM car_user)");
-
-if ($usersQ->count() > 0) {
-    $users = $usersQ->results();
-}
-
-
-foreach ($users as $u) {
-	echo "<p>Remove userID ". $u->userid."</p>";
-	deleteUsers(array($u->userid));
-
-}
 
 ?>
