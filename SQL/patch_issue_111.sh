10d80969635af966b16696def303ebd03b5c3424
Jim Boone <jim@unibrain.org>
2020-02-06 09:35:01 -0800

    Resolved issue #111

---
+++ SQL/patch_issue_111.sh
@@ -0,0 +1,9 @@
+#  UPDATE `cars` SET `ctime` = '2004-01-22 18:00:39' WHERE `cars`.`id` = 3
+
+IFS=,
+
+while read id date
+do
+	echo "UPDATE cars SET ctime = '${date}' WHERE cars.id = ${id}"
+
+done < patch_issue_111.csv
