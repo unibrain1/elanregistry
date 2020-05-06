c39676c4d612da263fcc976f75c391edfc46ffdd
Jim Boone <jim@unibrain.org>
2020-05-06 12:00:14 -0700

    Update  cars and cars_hist table for issue #118

--- SQL/patch_issue_118.sql
+++ SQL/patch_issue_118.sql
@@ -1,4 +1,7 @@
 /* Fix bad timestamps post mySQL 5.7 strict mode */
+
+/* Fix CARS */
+
 SET
     SQL_MODE
 = 'ALLOW_INVALID_DATES';
@@ -9,6 +12,25 @@ ALTER TABLE
 UPDATE
     `cars`
 SET
-`mtime` = '1970-01-01 00:00:01'
+    `mtime` = '1970-01-01 00:00:01'
 WHERE
     `mtime` = '0000-00-00 00:00:00';
+
+/* Fix CARS_HIST */
+
+ALTER TABLE
+    `cars_hist`
+    CHANGE `purchasedate` `purchasedate` DATE NULL,
+    CHANGE `solddate` `solddate` DATE NULL;
+UPDATE
+    `cars_hist`
+SET
+    `purchasedate` = NULL
+WHERE
+    `purchasedate` = '0000-00-00';
+UPDATE
+    `cars_hist`
+SET
+    `solddate` = NULL
+WHERE
+    `solddate` = '0000-00-00';
