d53ef7036d039d18b8bd03ef646f942378ed54fc
Jim Boone <jim@unibrain.org>
2020-05-13 15:33:41 -0700

    Removed unneeded comments and re-format

--- usersc/includes/head_tags.php
+++ usersc/includes/head_tags.php
@@ -1,14 +1,11 @@
-<!-- Facebook sharing meta tags (delete if you don't want them) -->
-<!-- Be sure to update this in .11 -->
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-<meta name="description" content="Registry for the Lotus Elan and Elan +2">
-<meta name="author" content="Jim Boone">
-<meta property="og:url"                content="https://www.elanregistry.org/" /> <!-- URL for website (link address) -->
-<meta property="og:type"               content="website" /> <!-- type of site -->
-<meta property="og:title"              content="Lotus Elan Registry" /> <!-- title of site (title of share) -->
-<meta property="og:description"        content="Registry for the Lotus Elan and Elan +2" /> <!-- description of site (text which appears when shari
+<meta charset    = "utf-8">
+<meta http-equiv = "X-UA-Compatible"    content = "IE = edge">
+<meta name       = "viewport"           content = "width   = device-width, initial-scale = 1, maximum-scale = 1">
+<meta name       = "description"        content = "Registry for the Lotus Elan and Elan +2">
+<meta name       = "author"             content = "Jim Boone">
+<meta property   = "og : url"           content = "https://www.elanregistry.org/" /> <!-- URL for website (link address) -->
+<meta property   = "og : type"          content = "website" /> <!-- type of site -->
+<meta property   = "og : title"         content = "Lotus Elan Registry" /> <!-- title of site (title of share) -->
+<meta property   = "og : description"   content = "Registry for the Lotus Elan and Elan +2" /> <!-- description of site (text which appears when shari
 ng) -->
-<meta property="og:image"              content="" /> <!-- URL for preview image -->	
-
+<meta property   = "og : image"         content = "" /> <!-- URL for preview image -->	
