48d46c758fb54e33f185871ceee77c11e2545b69
Jim Boone <jim@unibrain.org>
2020-05-20 15:33:42 -0700

    Display additional fields.  Disallow sorting history

--- app/car_details.php
+++ app/car_details.php
@@ -88,50 +88,62 @@ if (!empty($_GET)) {
       <div class="card-header"><h2><strong>Record Update History</strong></h2></div>
           <div class="card-body">
             <table id="historytable" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
-                <thead>
+              <thead>
+              <tr>
+                  <th>Operation</th>
+                  <th>Date Modified</th>
+                  <th>Year</th>
+                  <th>Type</th>
+                  <th>Chassis</th>
+                  <th>Series</th>
+                  <th>Variant</th>
+                  <th>Color</th>
+
+                  <th>Engine</th>
+                  <th>Purchase Date</th>
+                  <th>Sold Date</th>
+                  <th>Comments</th>
+
+                  <th>Image</th>
+                  <th>Owner</th>
+                  <th>City</th>
+                  <th>State</th>
+                  <th>Country</th>
+              </tr>
+              </thead>
+              <tbody>
+              <?php
+              //Cycle through users
+              foreach ($carHist as $v1) {
+                  ?>
                   <tr>
-                    <th>Operation</th>
-                    <th>Date Modified</th>
-                    <th>Year</th>
-                    <th>Type</th>
-                    <th>Chassis</th>
-                    <th>Series</th>
-                    <th>Variant</th>
-                    <th>Color</th>
-                    <th>Image</th>
-                    <th>Owner</th>
-                    <th>City</th>
-                    <th>State</th>
-                    <th>Country</th>
+                  <td><?=$v1->operation?></td>
+                  <td><?=$v1->timestamp?></td> 
+                  <td><?=$v1->year?></td>
+                  <td><?=$v1->type?></td>
+                  <td><?=$v1->chassis?></td>
+                  <td><?=$v1->series?></td>
+                  <td><?=$v1->variant?></td>
+                  <td><?=$v1->color?></td>
+
+                  <td><?=$v1->engine?></td>                        
+                  <td><?=$v1->purchasedate?></td>
+                  <td><?=$v1->solddate?></td>
+                  <td><?=$v1->comments?></td>
+
+                  <td> <?php
+                  if ($v1->image and file_exists($abs_us_root.$us_url_root."app/userimages/".$v1->image)) {
+                      echo '<img src='.$us_url_root.'app/userimages/thumbs/'.$v1->image.">";
+                  } ?>  </td>
+                  <td><?=$v1->lname?></td>
+                  <td><?=$v1->city?></td>
+                  <td><?=$v1->state?></td>
+                  <td><?=$v1->country?></td> 
                   </tr>
-                </thead>
-                <tbody>
-                  <?php
-                  //Cycle through history
-                  foreach ($carHist as $v1) {
-                      ?>
-                    <tr>
-                      <td><?=$v1->operation?></td>
-                      <td><?=$v1->timestamp?></td> 
-                      <td><?=$v1->year?></td>
-                      <td><?=$v1->type?></td>
-                      <td><?=$v1->chassis?></td>
-                      <td><?=$v1->series?></td>
-                      <td><?=$v1->variant?></td>
-                      <td><?=$v1->color?></td>
-                      <td> <?php
-                      if ($v1->image and file_exists($abs_us_root.$us_url_root."app/userimages/".$v1->image)) {
-                          echo '<img src='.$us_url_root.'app/userimages/thumbs/'.$v1->image.">";
-                      } ?>  </td>
-                      <td><?=$v1->fname?></td>
-                      <td><?=$v1->city?></td>
-                      <td><?=$v1->state?></td>
-                      <td><?=$v1->country?></td> 
-                    </tr>
-                  <?php
-                  } ?>
-                </tbody>
-              </table>
+              <?php
+              } ?>
+              </tbody>
+          </table>
           </div> <!-- card-body -->
         </div> <!-- card -->
     </div> <!-- well -->
@@ -155,7 +167,7 @@ $(document).ready(function()  {
 $(document).ready(function()  {
   var table =  $('#historytable').DataTable(
     {
-      "aaSorting": [[ 9, "dsc" ]],
+      "ordering": false,
       "scrollX": true
     });
 } );
