e918d5f3e0a51baef97b3ea35e65c31b5753d972
Jim Boone <jim@unibrain.org>
2020-06-08 17:00:35 -0700

    Delete the cars_hist entry for adding the vericode.  Limit to 1 email

--- verify/send_email.php
+++ verify/send_email.php
@@ -14,36 +14,40 @@ $base_url="http://localhost:8888";  // Testing only
 
 $verify_url=$base_url.$us_url_root."verify/verify_car.php";
 
-$carQ = $db->query("SELECT * FROM users_carsview WHERE mtime < DATE_SUB(NOW(), INTERVAL 16 YEAR) ORDER BY
-  `users_carsview`.`mtime` DESC LIMIT 30");
+$carQ = $db->query("SELECT * FROM users_carsview WHERE mtime < DATE_SUB(NOW(), INTERVAL 16 YEAR) ORDER BY `users_carsview`.`mtime` DESC LIMIT 3");
   $carData=$carQ->results();  // Results as an array
 
   // Set verification codes
 
-  foreach ($carData as $car) {
-      echo "<hr>Send email for car:".$car->id."<br>";
-      // Update the verification code
-      $verificationCode = md5(uniqid(rand(), true));
-      $db->query("UPDATE cars SET vericode = ? WHERE id = ?", [$verificationCode, $car->id]);
+foreach ($carData as $car) {
+    echo "<hr>Send email for car:".$car->id."<br>";
+    // Update the verification code
+    $verificationCode = md5(uniqid(rand(), true));
+    $db->query("UPDATE cars SET vericode = ? WHERE id = ?", [$verificationCode, $car->id]);
 
-      if ($car->image and file_exists($abs_us_root.$us_url_root.'app/userimages/'.$car->image)) {
-          $image = '<img src="'.$base_url.$us_url_root.'app/userimages/'.$car->image.'">';
-      } else {
-          $image = 'No image';
-      }
-      $verify_btn = $verify_url.'?code='.$verificationCode."&action=verify";
-      $sold_btn = $verify_url.'?code='.$verificationCode."&action=sold";
-      $edit_btn = $verify_url.'?code='.$verificationCode."&action=edit";
+    // Delete the cars_hist entry for adding the vericode
+    $id = $db->query('SELECT car_id, id, MAX(timestamp) AS max FROM cars_hist where car_id = ? GROUP BY id, car_id ORDER BY `max` DESC LIMIT 1', [$car->id])->first()->id;
+    $db->deleteById("cars_hist", $id);
 
 
-      $to= $car->email;
-      $subject = "Lotus Elan Registry - Request for Information Verification";
-    
-      // Get the email template
-      ob_start();
-      include('template.php');
-      $body = ob_get_contents();
-      ob_get_clean();
-      //echo $body;
-      email($to, $subject, $body); // PHPMailer
-  }
+    if ($car->image and file_exists($abs_us_root.$us_url_root.'app/userimages/'.$car->image)) {
+        $image = '<img src="'.$base_url.$us_url_root.'app/userimages/'.$car->image.'">';
+    } else {
+        $image = 'No image';
+    }
+    $verify_btn = $verify_url.'?code='.$verificationCode."&action=verify";
+    $sold_btn = $verify_url.'?code='.$verificationCode."&action=sold";
+    $edit_btn = $verify_url.'?code='.$verificationCode."&action=edit";
+
+
+    $to= $car->email;
+    $subject = "Lotus Elan Registry - Request for Information Verification";
+
+    // Get the email template
+    ob_start();
+    include('template.php');
+    $body = ob_get_contents();
+    ob_get_clean();
+    //echo $body;
+    email($to, $subject, $body); // PHPMailer
+}
