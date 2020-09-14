376340e9a9ee05b946d6cd91fef9b43bada41c98
Jim Boone <jim@unibrain.org>
2020-09-14 12:32:58 -0700

    Updates to car verification system

--- app/verify/send_email.php
+++ app/verify/send_email.php
@@ -13,11 +13,8 @@ $base_url = $query->first()->verify_url;
 
 $verify_url=$base_url.$us_url_root."app/verify/verify_car.php";
 
-// Get all the cars that haven't been modified in the last year.
-//  Webhost limits to < 250 emails per hour
-
-$carQ = $db->query("SELECT * FROM users_carsview WHERE  mtime  <  DATE_SUB(NOW(),INTERVAL 1 YEAR)  order by mtime ASC LIMIT 200");
-$carData=$carQ->results();
+$carQ = $db->query("SELECT * FROM users_carsview WHERE mtime < DATE_SUB(NOW(), INTERVAL 16 YEAR) ORDER BY `users_carsview`.`mtime` ASC LIMIT 1");
+  $carData=$carQ->results();  // Results as an array
 
   // Set verification codes
 
@@ -25,7 +22,12 @@ foreach ($carData as $car) {
     echo "<hr>Send email for car:".$car->id."<br>";
     // Update the verification code
     $verificationCode = md5(uniqid(rand(), true));
-    $db->query("SET @disable_triggers = 1;UPDATE cars SET vericode = ? WHERE id = ?", [$verificationCode, $car->id]);
+    $db->query("UPDATE cars SET vericode = ? WHERE id = ?", [$verificationCode, $car->id]);
+
+    // Delete the cars_hist entry for adding the vericode
+    $id = $db->query('SELECT car_id, id, MAX(timestamp) AS max FROM cars_hist where car_id = ? GROUP BY id, car_id ORDER BY `max` DESC LIMIT 1', [$car->id])->first()->id;
+    $db->deleteById("cars_hist", $id);
+
 
     if ($car->image and file_exists($abs_us_root.$us_url_root.'app/userimages/'.$car->image)) {
         $image = '<img src="'.$base_url.$us_url_root.'app/userimages/'.$car->image.'">';
