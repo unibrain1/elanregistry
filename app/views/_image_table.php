ab22388e417d878e21c47bc54be4167fa0b394fd
Jim Boone <jim@unibrain.org>
2020-10-16 10:10:59 -0700

    Update file selector with filename as an indicator that a file has been selected

--- app/views/_image_table.php
+++ app/views/_image_table.php
@@ -1,9 +1,11 @@
 
 <!-- Form for the 'image' -->
-<div class="custom-file">
-    <input type="file" class="custom-file-input" id="file" name="uploadedFile">
-    <label class="custom-file-label" for="file">Choose file</label>
-    <small id="fileHelp" class="form-text text-muted">Valid file types:  JPEG</small>
+<div class="input-group mb-3">
+    <div class="custom-file">
+        <input type="file" class="custom-file-input" id="file" name="file">
+        <label class="custom-file-label" for="file">Choose file</label>
+        <small id="fileHelp" class="form-text text-muted">Valid file types:  JPEG</small>
+    </div>
 </div>
 <!-- Add some space -->
 </br>
@@ -12,3 +14,14 @@ if ($cardetails['image']) { ?>
     <img class="card-img-top" src=<?= $us_url_root ?>app/userimages/<?= $cardetails['image'] ?> width='390'> <?php
 }
 ?>
+
+<script>
+    $('#file').on('change',function(){
+        //get the file name
+        var fileName = $(this).val();
+        // Remove c:\fakepath\
+        fileName = fileName.substring(fileName.lastIndexOf("\\") + 1, fileName.length);
+        //replace the "Choose a file" label
+        $(this).next('.custom-file-label').html(fileName);
+    })
+</script>
