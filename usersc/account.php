37bd9056611d5ed577abcf0117161070f5140c19
Jim Boone <jim@unibrain.org>
2020-10-16 11:14:12 -0700

    Removed datatables from page because it's not needed and was throwing a console error

--- usersc/account.php
+++ usersc/account.php
@@ -204,23 +204,7 @@ $(document).ready(function(){
 });
 </script>
 
-<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.18/css/jquery.dataTables.min.css"/>
-<script type="text/javascript" src="https://cdn.datatables.net/1.10.18/js/jquery.dataTables.min.js"></script>
 
-<script type="text/javascript">
-$(document).ready(function()  {
- var table =  $('#accounttable').DataTable();
-} );
-</script>
-
-<script type="text/javascript">
-$(document).ready(function()  {
-  var table =  $('#cartable').DataTable(
-    {
-      "scrollX": true
-    });
-} );
-</script>
 
 
 <!-- footers -->
