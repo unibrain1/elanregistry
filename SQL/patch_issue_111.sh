2eb5c315f9f882e494dfffe8a76cf8a73dcfa9a6
Jim Boone <jim@unibrain.org>
2020-10-19 11:29:08 -0700

    Remove assets that are no longer used or needed. FIX and SQL where patches for bugs, data issues, or new features that are incluced in base

--- SQL/patch_issue_111.sh
+++
@@ -1,9 +0,0 @@
-#  UPDATE `cars` SET `ctime` = '2004-01-22 18:00:39' WHERE `cars`.`id` = 3
-
-IFS=,
-
-while read id date
-do
-	echo "UPDATE cars SET ctime = '${date}' WHERE cars.id = ${id};"
-
-done < patch_issue_111.csv
