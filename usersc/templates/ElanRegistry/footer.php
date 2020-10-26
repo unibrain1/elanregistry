577f1da1fde8a5c9732dee3f938c2be74dcf0197
Jim Boone <jim@unibrain.org>
2020-10-26 11:43:18 -0700

    Added development footer to display content areas

--- usersc/templates/ElanRegistry/footer.php
+++ usersc/templates/ElanRegistry/footer.php
@@ -3,6 +3,7 @@ require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->temp
 require_once $abs_us_root . $us_url_root . 'users/includes/page_footer.php';
 ?>
 
+
 <script>
   var $hamburger = $(".hamburger");
   $hamburger.on("click", function(e) {
@@ -15,20 +16,31 @@ require_once $abs_us_root . $us_url_root . 'users/includes/page_footer.php';
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-7230761-6"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
   gtag('js', new Date());
 
   gtag('config', 'UA-7230761-6');
 </script>
 
 <div class="container">
-        <div class="row">
-                <div class="col-12 text-center">
-                        <footer><br>&copy;
-                          <?php echo date("Y"); ?>
-                            <?=$settings->copyright; ?></footer>
-                        <br>
-                </div>
-        </div>
+  <div class="row">
+    <div class="col-12 text-center">
+      <footer><br>&copy;
+        <?php echo date("Y"); ?>
+        <?= $settings->copyright; ?></footer>
+      <br>
+    </div>
+  </div>
 </div>
-<?php require_once($abs_us_root.$us_url_root.'users/includes/html_footer.php');?>
+<?php require_once($abs_us_root . $us_url_root . 'users/includes/html_footer.php'); ?>
+
+<script src="<?= $us_url_root ?>assets/js/show_struc.js"></script>
+
+<?php
+//optional
+if (file_exists($abs_us_root . $us_url_root . 'assets/js/show_struc.js')) { ?>
+  <script src="<?= $us_url_root ?>assets/js/show_struc.js"></script>
+<?php } ?>
