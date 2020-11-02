f80ac984b48b38a78b820fdcb692aae2ef06f0c4
Jim Boone <jim@unibrain.org>
2020-11-02 10:54:44 -0800

    Resolved #156

--- usersc/plugins/hooker/hooks/user_form_hook.php
+++ usersc/plugins/hooker/hooks/user_form_hook.php
@@ -1,4 +1,5 @@
-<?php if(count(get_included_files()) ==1) die(); //Direct Access Not Permitted Leave this line in place?>
+<?php if (count(get_included_files()) == 1) die(); //Direct Access Not Permitted Leave this line in place
+?>
 
 <?php
 /*
@@ -9,20 +10,52 @@ global $userId;
 
 $user_id = $userId;
 
-$userQ = $db->query("SELECT * FROM users_carsview WHERE user_id = ?", array($user_id));
+$userQ = $db->query("SELECT * FROM profiles WHERE id = ?", array($user_id));
 if ($userQ->count() > 0) {
-    $thatUser = $userQ->results();
+	$thatUser = $userQ->results();
+}
+
+$carQ = $db->query("SELECT * FROM users_carsview WHERE user_id = ?", array($user_id));
+if ($carQ->count() > 0) {
+	$thatCar = $carQ->results();
 }
 
 ?>
-<table id="accounttable" width="100%" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">	
-	<tr ><td ><strong>City            : </strong></td><td ><?= html_entity_decode($thatUser[0]->city);?></td></tr>
-	<tr ><td ><strong>State           : </strong></td><td ><?= html_entity_decode($thatUser[0]->state);?></td></tr>
-	<tr ><td ><strong>Country         : </strong></td><td ><?= html_entity_decode($thatUser[0]->country);?></td></tr>
-	<tr ><td ><strong>LAT	          : </strong></td><td ><?= html_entity_decode($thatUser[0]->lat);?></td></tr>
-	<tr ><td ><strong>LON	          : </strong></td><td ><?= html_entity_decode($thatUser[0]->lon);?></td></tr>
-	<tr ><td ><strong>CAR ID          : </strong></td><td ><?= html_entity_decode($thatUser[0]->id);?></td></tr>
-	<tr ><td ><strong>YEAR            : </strong></td><td ><?= html_entity_decode($thatUser[0]->year);?></td></tr>
-	<tr ><td ><strong>TYPE			  : </strong></td><td ><?= html_entity_decode($thatUser[0]->type);?></td></tr>
-	<tr ><td ><strong>CHASSIS		  : </strong></td><td ><?= html_entity_decode($thatUser[0]->chassis);?></td></tr>
+<table id="accounttable" width="100%" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+	<tr>
+		<td><strong>City : </strong></td>
+		<td><?= html_entity_decode($thatUser[0]->city); ?></td>
+	</tr>
+	<tr>
+		<td><strong>State : </strong></td>
+		<td><?= html_entity_decode($thatUser[0]->state); ?></td>
+	</tr>
+	<tr>
+		<td><strong>Country : </strong></td>
+		<td><?= html_entity_decode($thatUser[0]->country); ?></td>
+	</tr>
+	<tr>
+		<td><strong>LAT : </strong></td>
+		<td><?= html_entity_decode($thatUser[0]->lat); ?></td>
+	</tr>
+	<tr>
+		<td><strong>LON : </strong></td>
+		<td><?= html_entity_decode($thatUser[0]->lon); ?></td>
+	</tr>
+	<tr>
+		<td><strong>CAR ID : </strong></td>
+		<td><?= html_entity_decode($thatUser[0]->id); ?></td>
+	</tr>
+	<tr>
+		<td><strong>YEAR : </strong></td>
+		<td><?= html_entity_decode($thatCar[0]->year); ?></td>
+	</tr>
+	<tr>
+		<td><strong>TYPE : </strong></td>
+		<td><?= html_entity_decode($thatCar[0]->type); ?></td>
+	</tr>
+	<tr>
+		<td><strong>CHASSIS : </strong></td>
+		<td><?= html_entity_decode($thatCar[0]->chassis); ?></td>
+	</tr>
 </table>
