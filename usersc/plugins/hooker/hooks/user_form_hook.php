ef50cf00907d438096b8f97853657a2bcc6b6110
Jim Boone <jim@unibrain.org>
2020-11-03 08:19:34 -0800

    Removed depricated features

--- usersc/plugins/hooker/hooks/user_form_hook.php
+++ usersc/plugins/hooker/hooks/user_form_hook.php
@@ -1,4 +1,6 @@
-<?php if (count(get_included_files()) == 1) die(); //Direct Access Not Permitted Leave this line in place
+<?php if (count(get_included_files()) == 1) {
+	die();
+} //Direct Access Not Permitted Leave this line in place
 ?>
 
 <?php
@@ -10,7 +12,7 @@ global $userId;
 
 $user_id = $userId;
 
-$userQ = $db->query("SELECT * FROM profiles WHERE id = ?", array($user_id));
+$userQ = $db->query("SELECT * FROM profiles WHERE user_id = ?", array($user_id));
 if ($userQ->count() > 0) {
 	$thatUser = $userQ->results();
 }
@@ -21,41 +23,41 @@ if ($carQ->count() > 0) {
 }
 
 ?>
-<table id="accounttable" width="100%" class="table table-striped table-bordered table-sm" cellspacing="0" width="100%">
+<table id="accounttable" class="table table-striped table-bordered table-sm">
 	<tr>
 		<td><strong>City : </strong></td>
-		<td><?= html_entity_decode($thatUser[0]->city); ?></td>
+		<td><?= $thatUser[0]->city; ?></td>
 	</tr>
 	<tr>
 		<td><strong>State : </strong></td>
-		<td><?= html_entity_decode($thatUser[0]->state); ?></td>
+		<td><?= $thatUser[0]->state; ?></td>
 	</tr>
 	<tr>
 		<td><strong>Country : </strong></td>
-		<td><?= html_entity_decode($thatUser[0]->country); ?></td>
+		<td><?= $thatUser[0]->country; ?></td>
 	</tr>
 	<tr>
 		<td><strong>LAT : </strong></td>
-		<td><?= html_entity_decode($thatUser[0]->lat); ?></td>
+		<td><?= $thatUser[0]->lat; ?></td>
 	</tr>
 	<tr>
 		<td><strong>LON : </strong></td>
-		<td><?= html_entity_decode($thatUser[0]->lon); ?></td>
+		<td><?= $thatUser[0]->lon; ?></td>
 	</tr>
 	<tr>
 		<td><strong>CAR ID : </strong></td>
-		<td><?= html_entity_decode($thatUser[0]->id); ?></td>
+		<td><?= $thatUser[0]->id; ?></td>
 	</tr>
 	<tr>
 		<td><strong>YEAR : </strong></td>
-		<td><?= html_entity_decode($thatCar[0]->year); ?></td>
+		<td><?= $thatCar[0]->year; ?></td>
 	</tr>
 	<tr>
 		<td><strong>TYPE : </strong></td>
-		<td><?= html_entity_decode($thatCar[0]->type); ?></td>
+		<td><?= $thatCar[0]->type; ?></td>
 	</tr>
 	<tr>
 		<td><strong>CHASSIS : </strong></td>
-		<td><?= html_entity_decode($thatCar[0]->chassis); ?></td>
+		<td><?= $thatCar[0]->chassis; ?></td>
 	</tr>
 </table>
