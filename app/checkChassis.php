18bbb61ddcfa30d02e72fef668c5daa2678ddd6a
Jim Boone <jim@unibrain.org>
2020-11-23 11:35:47 -0800

    Added car validation information to edit_car  - Validates year, model, and chassis number  - Verifies chassis number is not taken
    Moved car definition to application tree
    
    Rename views to be more descriptive

---
+++ app/checkChassis.php
@@ -0,0 +1,33 @@
+<?php
+
+// Check to see if the chassis number is taken
+require_once '../users/init.php';
+// Get the DB
+$db = DB::getInstance();
+
+if (isset($_POST['command'])) {
+    $command = $_POST['command'];
+
+    switch ($command) {
+        case 'chassis_check':
+            $year = $_POST['year'];
+            list($series, $variant, $type) = explode('|', $_POST['model']);
+            $chassis = $_POST['chassis'];
+
+            $carQ = $db->query('SELECT * FROM cars WHERE year=? AND type = ? AND chassis=?', [$year, $type, $chassis]);
+
+            // Did it return anything?
+            if ($carQ->count() !== 0) {
+                // Yes it did
+                echo "taken";
+            } else {
+                echo 'not_taken';
+            }
+            break;
+
+        default:
+            echo "Called with invalid CMD";
+            break;
+    }
+}
+exit();
---
+++ app/views/_image_upload.php
@@ -0,0 +1,36 @@
+<div class="card card-default">
+    <div class="card-header">
+        <h2><strong>Photos</strong></h2>
+    </div>
+    <div class="card-body">
+
+        <!-- Form for the 'image' -->
+        <div class="input-group mb-3">
+            <div class="custom-file">
+                <input type="file" class="custom-file-input" id="file" name="file">
+                <label class="custom-file-label" for="file">Choose file</label>
+                <small id="fileHelp" class="form-text text-muted">Valid file types: JPEG</small>
+            </div>
+        </div>
+        <!-- Add some space -->
+        <br>
+        <?php
+        if ($cardetails['image']) { ?>
+            <img class="card-img-top" alt="mycar" src=<?= $us_url_root ?>app/userimages/<?= $cardetails['image'] ?> width='390'>
+        <?php
+        }
+        ?>
+
+        <script>
+            // Update file select box with filename
+            $('#file').on('change', function() {
+                //get the file name
+                var fileName = $(this).val();
+                // Remove c:\fakepath\
+                fileName = fileName.substring(fileName.lastIndexOf("\\") + 1, fileName.length);
+                //replace the "Choose a file" label
+                $(this).next('.custom-file-label').html(fileName);
+            })
+        </script>
+    </div>
+</div> <!-- card -->
