44321e826bba16ec9144f79977badef560297c48
Jim Boone <jim@unibrain.org>
2021-01-04 12:34:27 -0800

    Change cars structure for longer list of images

--- SQL/create_image_table.sql
+++
@@ -1,6 +0,0 @@
--- Create images table
-
-CREATE TABLE  `images` ( `id` INT NOT NULL AUTO_INCREMENT , `ctime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `carid` INT(10) NOT NULL , `featured` BOOLEAN NULL DEFAULT NULL , `image` VARCHAR(100) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-
-
-
