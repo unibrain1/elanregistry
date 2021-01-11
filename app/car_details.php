b56008741f37f8487e333469486b94d561161337
Jim Boone <jim@unibrain.org>
2021-01-11 15:38:02 -0800

    Add owner to owner contact

--- app/car_details.php
+++ app/car_details.php
@@ -59,11 +59,40 @@ if (!empty($_GET)) {
           <!-- Image -->
           <div class="card card-default">
             <div class="card-header">
-              <h2><strong>The Car</strong></h2>
+              <div class="form-group row">
+                <div class='col-md-7'>
+                  <h2><strong>Car Information</strong></h2>
+                </div>
+                <div class='col-md-5 text-right'>
+                  <?php
+                  if (isset($user) && $user->isLoggedIn()) {
+                    if ($user->data()->id === $carData[0]->user_id) { ?>
+                      <form method='POST' action=<?= $us_url_root . 'app/edit_car.php' ?>>
+                        <input type="hidden" name="csrf" value="<?= Token::generate(); ?>" />
+                        <input type="hidden" name="action" value="update_car" />
+                        <input type="hidden" name="carid" id='carid' value="<?= $carData[0]->id ?>" />
+                        <button class='btn btn-block btn-success' type="submit">Update</button>
+                      </form>
+                    <?php
+                    } else {
+                    ?>
+                      <form method='POST' action=<?= $us_url_root . 'app/contact_owner.php' ?>>
+                        <input type="hidden" name="csrf" value="<?= Token::generate(); ?>" />
+                        <input type="hidden" name="action" value="contact_owner" />
+                        <input type="hidden" name="carid" id='carid' value="<?= $carData[0]->id ?>" />
+                        <button class='btn btn-block btn-success' type="submit">Contact Owner</button>
+                      </form>
+                  <?php
+                    }
+                  } else {
+                    echo "<input type='hidden' name='carid' id='carid' value='" . $car->id . "' />";
+                  }
+                  ?>
+                </div>
+              </div>
             </div>
             <div class="card-body">
               <?php
-
               if ($carData[0]->image && file_exists($abs_us_root . $us_url_root . "app/userimages/" . $carData[0]->image)) {
                 echo  '<img alt="the elan" class="card-img-top" src="' . $us_url_root . 'app/userimages/' . $carData[0]->image . '">';
               }
@@ -77,188 +106,187 @@ if (!empty($_GET)) {
           <div class="card card-default">
             <div class="card-header">
               <h2><strong>Car Information</strong></h2>
-            </div>
-            <div class="card-body">
-              <table id="cartable" class="table table-striped table-bordered table-sm" aria-describedby="card-header">
-                <tr class="table-success">
-                  <th scope=column><strong>Car ID:</strong></th>
-                  <th scope=column><?= $carData[0]->id ?></th>
-                </tr>
-                <tr>
-                  <td><strong>Series:</strong></td>
-                  <td><?= $carData[0]->series ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Variant:</strong></td>
-                  <td><?= $carData[0]->variant ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Model:</strong></td>
-                  <td><?= $carData[0]->model ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Year:</strong></td>
-                  <td><?= $carData[0]->year ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Type:</strong></td>
-                  <td><?= $carData[0]->type ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Chassis :</strong></td>
-                  <td><?= $carData[0]->chassis ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Color:</strong></td>
-                  <td><?= $carData[0]->color ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Engine :</strong></td>
-                  <td><?= $carData[0]->engine ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Purchase Date:</strong></td>
-                  <td><?= $carData[0]->purchasedate ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Sold Date :</strong></td>
-                  <td><?= $carData[0]->solddate ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Comments:</strong></td>
-                  <td><?= $carData[0]->comments ?></td>
-                </tr>
-                <tr class="table-success">
-                  <td><strong>Owner ID:</strong></td>
-                  <td><?= $carData[0]->user_id ?></td>
-                </tr>
-                <tr>
-                  <td><strong>First name:</strong></td>
-                  <td><?= ucfirst($carData[0]->fname) ?></td>
-                </tr>
-                <tr>
-                  <td><strong>City</strong></td>
-                  <td><?= html_entity_decode($carData[0]->city); ?></td>
-                </tr>
-                <tr>
-                  <td><strong>State:</strong></td>
-                  <td><?= html_entity_decode($carData[0]->state); ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Country:</strong></td>
-                  <td><?= html_entity_decode($carData[0]->country); ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Member Since:</strong></td>
-                  <td><?= $signupdate ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Record Created:</strong></td>
-                  <td><?= $carData[0]->ctime ?></td>
-                </tr>
-                <tr>
-                  <td><strong>Record Modified:</strong></td>
-                  <td><?= $carData[0]->mtime ?></td>
-                </tr>
-                <?php
-                if (!empty($carData[0]->website)) {
-                ?>
-                  <tr>
-                    <td><strong>Website:</strong></td>
-                    <td> <a target="_blank" href="<?= $carData[0]->website ?>">Website</a></td>
+              <div class="card-body">
+                <table id="cartable" class="table table-striped table-bordered table-sm" aria-describedby="card-header">
+                  <tr class="table-success">
+                    <th scope=column><strong>Car ID:</strong></th>
+                    <th scope=column><?= $carData[0]->id ?></th>
                   </tr>
-                <?php
-                }
-                ?>
-                <?php
-                if ($carFactory !== FALSE) { ?>
-
-                  <tr class="table-info">
-                    <td colspan=2><strong>Factory Data - <small>I've lost track of where this data originated and it may be incomplete, inaccurate, false, or just plain made up.</small></strong></td>
+                  <tr>
+                    <td><strong>Series:</strong></td>
+                    <td><?= $carData[0]->series ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Variant:</strong></td>
+                    <td><?= $carData[0]->variant ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Model:</strong></td>
+                    <td><?= $carData[0]->model ?></td>
                   </tr>
                   <tr>
                     <td><strong>Year:</strong></td>
-                    <td><?= $carFactory[0]->year ?></td>
-                  </tr>
-                  <tr>
-                    <td><strong>Month:</strong></td>
-                    <td><?= $carFactory[0]->month ?></td>
-                  </tr>
-                  <tr>
-                    <td><strong>Production Batch:</strong></td>
-                    <td><?= $carFactory[0]->batch ?></td>
+                    <td><?= $carData[0]->year ?></td>
                   </tr>
                   <tr>
                     <td><strong>Type:</strong></td>
-                    <td><?= $carFactory[0]->type ?></td>
+                    <td><?= $carData[0]->type ?></td>
                   </tr>
                   <tr>
-                    <td><strong>Chassis:</strong></td>
-                    <td><?= $carFactory[0]->serial ?></td>
-                  </tr>
-                  <tr>
-                    <td><strong>Suffix:</strong></td>
-                    <td><?= $carFactory[0]->suffix ?></td>
-                  </tr>
-                  <tr>
-                    <td><strong>Engine:</strong></td>
-                    <td><?= $carFactory[0]->engineletter ?><?= $carFactory[0]->enginenumber ?></td>
-                  </tr>
-                  <tr>
-                    <td><strong>Gearbox:</strong></td>
-                    <td><?= $carFactory[0]->gearbox ?></td>
+                    <td><strong>Chassis :</strong></td>
+                    <td><?= $carData[0]->chassis ?></td>
                   </tr>
                   <tr>
                     <td><strong>Color:</strong></td>
-                    <td><?= $carFactory[0]->color ?></td>
+                    <td><?= $carData[0]->color ?></td>
                   </tr>
                   <tr>
-                    <td><strong>Build Date:</strong></td>
-                    <td><?= $carFactory[0]->builddate ?></td>
+                    <td><strong>Engine :</strong></td>
+                    <td><?= $carData[0]->engine ?></td>
                   </tr>
                   <tr>
-                    <td><strong>Notes:</strong></td>
-                    <td><?= $carFactory[0]->note ?></td>
+                    <td><strong>Purchase Date:</strong></td>
+                    <td><?= $carData[0]->purchasedate ?></td>
                   </tr>
-                <?php } ?>
+                  <tr>
+                    <td><strong>Sold Date :</strong></td>
+                    <td><?= $carData[0]->solddate ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Comments:</strong></td>
+                    <td><?= $carData[0]->comments ?></td>
+                  </tr>
+                  <tr class="table-success">
+                    <td><strong>Owner ID:</strong></td>
+                    <td><?= $carData[0]->user_id ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>First name:</strong></td>
+                    <td><?= ucfirst($carData[0]->fname) ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>City</strong></td>
+                    <td><?= html_entity_decode($carData[0]->city); ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>State:</strong></td>
+                    <td><?= html_entity_decode($carData[0]->state); ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Country:</strong></td>
+                    <td><?= html_entity_decode($carData[0]->country); ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Member Since:</strong></td>
+                    <td><?= $signupdate ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Record Created:</strong></td>
+                    <td><?= $carData[0]->ctime ?></td>
+                  </tr>
+                  <tr>
+                    <td><strong>Record Modified:</strong></td>
+                    <td><?= $carData[0]->mtime ?></td>
+                  </tr>
+                  <?php
+                  if (!empty($carData[0]->website)) {
+                  ?>
+                    <tr>
+                      <td><strong>Website:</strong></td>
+                      <td> <a target="_blank" href="<?= $carData[0]->website ?>">Website</a></td>
+                    </tr>
+                  <?php
+                  }
+                  ?>
+                  <?php
+                  if ($carFactory !== FALSE) { ?>
 
-              </table>
+                    <tr class="table-info">
+                      <td colspan=2><strong>Factory Data - <small>I've lost track of where this data originated and it may be incomplete, inaccurate, false, or just plain made up.</small></strong></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Year:</strong></td>
+                      <td><?= $carFactory[0]->year ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Month:</strong></td>
+                      <td><?= $carFactory[0]->month ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Production Batch:</strong></td>
+                      <td><?= $carFactory[0]->batch ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Type:</strong></td>
+                      <td><?= $carFactory[0]->type ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Chassis:</strong></td>
+                      <td><?= $carFactory[0]->serial ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Suffix:</strong></td>
+                      <td><?= $carFactory[0]->suffix ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Engine:</strong></td>
+                      <td><?= $carFactory[0]->engineletter ?><?= $carFactory[0]->enginenumber ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Gearbox:</strong></td>
+                      <td><?= $carFactory[0]->gearbox ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Color:</strong></td>
+                      <td><?= $carFactory[0]->color ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Build Date:</strong></td>
+                      <td><?= $carFactory[0]->builddate ?></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Notes:</strong></td>
+                      <td><?= $carFactory[0]->note ?></td>
+                    </tr>
+                  <?php } ?>
+
+                </table>
+              </div>
             </div>
-          </div>
-        </div> <!-- col-xs-12 col-md-6 -->
-      </div> <!-- row -->
-      <br>
-      <div class="row">
-        <div class="col">
-          <!-- Image -->
-          <div class="card border-success">
-            <div class="card-header">
-              <h2><strong>Record Update History</strong></h2>
-            </div>
-            <div class="card-body">
-              <?php include($abs_us_root . $us_url_root . 'app/views/_car_history.php'); ?>
-            </div> <!-- card-body -->
-          </div> <!-- card -->
-        </div> <!-- col -->
-      </div> <!-- row -->
-    </div> <!-- well -->
-  </div> <!-- container -->
-</div> <!-- page-wrapper -->
+          </div> <!-- col-xs-12 col-md-6 -->
+        </div> <!-- row -->
+        <br>
+        <div class="row">
+          <div class="col">
+            <!-- Image -->
+            <div class="card border-success">
+              <div class="card-header">
+                <h2><strong>Record Update History</strong></h2>
+              </div>
+              <div class="card-body">
+                <?php include($abs_us_root . $us_url_root . 'app/views/_car_history.php'); ?>
+              </div> <!-- card-body -->
+            </div> <!-- card -->
+          </div> <!-- col -->
+        </div> <!-- row -->
+      </div> <!-- well -->
+    </div> <!-- container -->
+  </div> <!-- page-wrapper -->
 
 
-<!-- Table Sorting and Such -->
-<?php require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->template . '/datatables.php'; ?>
+  <!-- Table Sorting and Such -->
+  <?php require_once $abs_us_root . $us_url_root . 'usersc/templates/' . $settings->template . '/datatables.php'; ?>
 
-<script type="text/javascript">
-  $(document).ready(function() {
-    var table = $('#historytable').DataTable({
-      "ordering": false,
-      "scrollX": true
+  <script type="text/javascript">
+    $(document).ready(function() {
+      var table = $('#historytable').DataTable({
+        "ordering": false,
+        "scrollX": true
+      });
     });
-  });
-</script>
+  </script>
 
-<!-- footers -->
-<?php
-require_once $abs_us_root . $us_url_root . 'users/includes/html_footer.php'; //custom template footer
-?>
+  <!-- footers -->
+  <?php
+  require_once $abs_us_root . $us_url_root . 'users/includes/html_footer.php'; //custom template footer
+  ?>
