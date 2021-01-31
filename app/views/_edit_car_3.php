c6c205490c3383235d5a0441a4d1fcea26c48828
Jim Boone <jim@unibrain.org>
2021-01-31 15:22:47 -0800

    Allow multiple picture upload.

---
+++ app/views/_edit_car_3.php
@@ -0,0 +1,19 @@
+<div class='row'>
+    <div class='col-3'>
+        <ul>
+            <li>Maximum of 5 photos</li>
+            <li>Maximum size 2 MB each</li>
+            <li>Photos only</li>
+            <li>Drag and Drop to reorder the photos</li>
+        </ul>
+    </div>
+    <div class='col-9'>
+        <div class="dropzone dz-clickable" id="myDrop">
+            <div class="dz-default dz-message" data-dz-message="">
+                <span>Drop files here to upload</span>
+            </div>
+        </div>
+    </div>
+
+
+</div>
