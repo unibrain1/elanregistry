6d1c2664a0c7914797486d0b75fe8196d511faa5
Jim Boone <jim@unibrain.org>
2021-02-03 11:41:11 -0800

    Fixed mobile/xs display issues

--- app/views/_edit_car_3.php
+++ app/views/_edit_car_3.php
@@ -1,5 +1,5 @@
 <div class='row'>
-    <div class='col-3'>
+    <div class='col-md-3 col-xs-12 '>
         <ul>
             <li>Maximum of 6 photos</li>
             <li>Maximum size 2 MB each</li>
@@ -7,9 +7,9 @@
             <li>Drag and Drop to reorder the photos</li>
         </ul>
     </div>
-    <div class='col-9'>
-        <div class="dropzone dz-clickable" id="myDrop">
-            <div class="dz-default dz-message" data-dz-message="">
+    <div class='col-md-9 col-xs-12'>
+        <div class='dropzone dz-clickable' id='myDrop'>
+            <div class='dz-default dz-message' data-dz-message=''>
                 <span>Drop files here to upload</span>
             </div>
         </div>
