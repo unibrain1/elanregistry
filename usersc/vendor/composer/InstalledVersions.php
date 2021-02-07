d9f3b707ff8154604d0cc1b43fc20d7fb93c28ac
Jim Boone <jim@unibrain.org>
2021-02-07 10:01:17 -0800

    Update dropzone tp v5.7.4

--- usersc/vendor/composer/InstalledVersions.php
+++ usersc/vendor/composer/InstalledVersions.php
@@ -19,19 +19,19 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '46ff82761a440014c3d920d7e150979bd4fc4e7e',
+    'reference' => '4d9b1819b729ae7382b949dd1b21aaec8fa57e20',
     'name' => 'jimboone/elan_registry',
   ),
   'versions' => 
   array (
     'enyo/dropzone' => 
     array (
-      'pretty_version' => 'v5.7.2',
-      'version' => '5.7.2.0',
+      'pretty_version' => 'v5.7.4',
+      'version' => '5.7.4.0',
       'aliases' => 
       array (
       ),
-      'reference' => '78424e9c97b569e698cc9175e9bc424168219162',
+      'reference' => '92c246b8a6d58cb86098a4195378baa9e32a539b',
     ),
     'jimboone/elan_registry' => 
     array (
@@ -40,7 +40,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '46ff82761a440014c3d920d7e150979bd4fc4e7e',
+      'reference' => '4d9b1819b729ae7382b949dd1b21aaec8fa57e20',
     ),
     'johnathanmiller/secure-env-php' => 
     array (
