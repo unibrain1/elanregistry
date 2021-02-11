bcde462db5533be8a3e8dabf45b9496212acb5f4
Jim Boone <jim@unibrain.org>
2021-02-11 15:01:56 -0800

    Remove dropzone download.  Using CDN

--- usersc/vendor/composer/installed.php
+++ usersc/vendor/composer/installed.php
@@ -6,20 +6,11 @@
     'aliases' => 
     array (
     ),
-    'reference' => '4d9b1819b729ae7382b949dd1b21aaec8fa57e20',
+    'reference' => '7263461322f7bb62cee7222e63b3b8dab015beba',
     'name' => 'jimboone/elan_registry',
   ),
   'versions' => 
   array (
-    'enyo/dropzone' => 
-    array (
-      'pretty_version' => 'v5.7.4',
-      'version' => '5.7.4.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '92c246b8a6d58cb86098a4195378baa9e32a539b',
-    ),
     'jimboone/elan_registry' => 
     array (
       'pretty_version' => 'dev-master',
@@ -27,7 +18,7 @@
       'aliases' => 
       array (
       ),
-      'reference' => '4d9b1819b729ae7382b949dd1b21aaec8fa57e20',
+      'reference' => '7263461322f7bb62cee7222e63b3b8dab015beba',
     ),
     'johnathanmiller/secure-env-php' => 
     array (
