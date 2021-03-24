6f1450d59488ead71cd7ad9d5d004c58b847353e
Jim Boone <jim@unibrain.org>
2021-03-24 09:19:47 -0700

    Removed validate.php - No longer used.

--- usersc/account.php
+++ usersc/account.php
@@ -1,8 +1,6 @@
 <?php
 require_once '../users/init.php';
 require_once $abs_us_root . $us_url_root . 'users/includes/template/prep.php';
-require_once '../app/validate.php';
-
 
 if (!securePage($_SERVER['PHP_SELF'])) {
 	die();
