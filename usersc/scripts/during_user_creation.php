c25b3c1af74b2a9cc16326c70d2f1ea7ac123239
Jim Boone <jim@unibrain.org>
2021-08-12 14:46:44 -0700

    Uninitialized city,state,country in geocode

--- usersc/scripts/during_user_creation.php
+++ usersc/scripts/during_user_creation.php
@@ -13,23 +13,26 @@
 
 // Create a profiles entry if it does not exist (it shouldn't)
 
-$check = $db->query("SELECT id FROM profiles WHERE user_id = ?", $theNewId)->count();
+$check = $db->query("SELECT id FROM profiles WHERE user_id = ?", [$theNewId])->count();
 if ($check < 1) {
     $now = new DateTime();
-    $db->insert('profiles', ['user_id'=>$theNewId,'bio'=>"automatically added - ".$now->format('Y-m-d H:i:s')]);
+    $db->insert('profiles', ['user_id' => $theNewId, 'bio' => "automatically added - " . $now->format('Y-m-d H:i:s')]);
 }
 
 
 // The format of the array is ['column_name'=>Data_for_column]
+$city = Input::get('city');
+$state = Input::get('state');
+$country = Input::get('country');
 
-$db->update('profiles', ["user_id","=",$theNewId], ['city'=>Input::get('city')]);
-$db->update('profiles', ["user_id","=",$theNewId], ['state'=>Input::get('state')]);
-$db->update('profiles', ["user_id","=",$theNewId], ['country'=>Input::get('country')]);
+$db->update('profiles', ["user_id", "=", $theNewId], ['city' => $city]);
+$db->update('profiles', ["user_id", "=", $theNewId], ['state' => $state]);
+$db->update('profiles', ["user_id", "=", $theNewId], ['country' => $country]);
 
 //   Update geolocation
-        $fields = [];
-include($abs_us_root.$us_url_root.'app/views/_geolocate.php');
-$db->update('profiles', ["user_id","=",$theNewId], $fields);
+$fields = [];
+include($abs_us_root . $us_url_root . 'app/views/_geolocate.php');
+$db->update('profiles', ["user_id", "=", $theNewId], $fields);
 
 // Even if you do not want to add additional fields to the the join form, this is a great opportunity to add this user to another database table.
 // Get creative!
