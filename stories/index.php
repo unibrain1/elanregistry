8c0c84452486ad5fbb94dc46427aca324a3cb6b1
Jim Boone <jim@unibrain.org>
2022-03-28 09:59:56 -0700

    New owner story

--- stories/index.php
+++ stories/index.php
@@ -14,18 +14,41 @@ require_once $abs_us_root . $us_url_root . 'users/includes/template/prep.php';
             <div class="col">
                 <div class="card bg-secondary">
                     <div class="card-header">
-                        <h2>User submitted car histories and personal stories</h2>
+                        <h2>Owner submitted histories and stories</h2>
                     </div>
                     <div class="card-body">
-                        <ul class="list-group">
-                            <li class="list-group-item d-flex justify-content-between align-items-center"><a href="SGO_2F/index.php">The story of SGO 2F: 50/0164</a></li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center"><a href="brian_walton/index.php">Elan Experimental Rally Car: 36/6086</a></li>
-                            <li class="list-group-item d-flex justify-content-between align-items-center"><a href="type26registry/index.html">Archive of www.type26registry.com</a> - This is probably incomplete.</li>
-                        </ul>
+
+                        <table class='table table-striped table-bordered table-sm'>
+                            <tr class='table-success'>
+                                <th>Article</td>
+                                <th>Comments</td>
+                            </tr>
+                            <tr>
+                                <td> <a href="SGO_2F/index.php">The story of SGO 2F: 50/0164</a></td>
+                                <td></td>
+                            </tr>
+
+                            <tr>
+                                <td><a href="brian_walton/index.php">Elan Experimental Rally Car: 36/6086</a></td>
+                                <td></td>
+                            </tr>
+
+                            <tr>
+                                <td> <a href='<?= $us_url_root ?>app/assets/docs/embed.php?doc=Mag _issue_50_p12-15_Barry-Shapecraft.pdf'>Shapecraft Elan - 26/4992</a>
+                                </td>
+                                <td><a href="http://www.historiclotusclub.uk/the-magazine/no-50-spring-2022">Historic Lotus Racing magazine, No. 50, Spring 2022</a></td>
+                            </tr>
+
+                            <tr>
+                                <td> <a href="type26registry/index.html">Archive of www.type26registry.com</a> </td>
+                                <td>This is an incomplete archive.</td>
+                            </tr>
+
+                        </table>
                     </div>
                 </div>
             </div> <!-- /.col -->
-            <div class="col">
+            <div class=" col">
                 <div class="card bg-secondary">
                     <div class="card-header">
                         <h2>Elan Manuals</h2>
